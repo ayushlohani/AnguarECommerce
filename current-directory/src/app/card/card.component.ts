d9485f0166122c16634c01713830d75d628c329f
Ayush Lohani <lohaniayush97@gmail.com>
2024-05-09 11:25:33 +0530

    addcart functionality

--- current-directory/src/app/card/card.component.ts
+++ current-directory/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-card',
@@ -6,14 +7,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() id:number=0;
   @Input() Title:string="Unknown";
   @Input() Price:Number=0;
   @Input() Rating:number=4.5;
   @Input() Description:string="A description of item......";
   @Input() ImgUrl:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBidlKMEwiSWGd-ABQIybSBaIAHQJvtTxVAKv2qW90xD_pSbALRI5vKCHwnulHfK3O-e0&usqp=CAU"
-  constructor() { }
+  constructor(private item:ItemService) { }
 
   ngOnInit(): void {
   }
 
+  add(val:number){
+    if(val>0){
+      this.item.AddCart(val);
+    }
+    console.log(this.item.cart);
+  }
 }
